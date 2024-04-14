badf01c4f3b255e50941fdf2a8b954d87380e159
sojitrayash <129203193+sojitrayash@users.noreply.github.com>
2024-04-14 16:22:01 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -124,3 +124,29 @@
     
 })(jQuery);
 
+let slideIndex = 0;
+const slides = document.querySelectorAll('.slider img');
+
+function showSlide(index) {
+  if (index < 0) {
+    slideIndex = slides.length - 1;
+  } else if (index >= slides.length) {
+    slideIndex = 0;
+  }
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+
+  slides[slideIndex].style.display = 'block';
+}
+
+function nextSlide() {
+  showSlide(++slideIndex);
+}
+
+function prevSlide() {
+  showSlide(--slideIndex);
+}
+
+showSlide(slideIndex);
